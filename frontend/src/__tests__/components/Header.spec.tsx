89ff34823fd55d11973ddadb850151aa10750dfc
gabriel-fz <gabrielfzancanela@gmail.com>
2020-12-28 18:14:49 -0300

    teste do componente de header concluido

---
+++ frontend/src/__tests__/components/Header.spec.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Header from '../../components/Header';
+
+jest.mock('react-router-dom', () => {
+  return {
+    Link: ({ children, ...rest }: { children: React.ReactNode }) => (
+      <a {...rest}>{children}</a>
+    ),
+  };
+});
+
+describe('Header Component', () => {
+  it('Espero que component Header seja renderizado', () => {
+    const { getByText } = render(
+      <Header title="Itens analizados" path="/records">
+        Ver comentários
+      </Header>,
+    );
+
+    expect(getByText('Itens analizados')).toBeTruthy();
+    expect(getByText('Ver comentários')).toHaveAttribute('to', '/records');
+  });
+});
